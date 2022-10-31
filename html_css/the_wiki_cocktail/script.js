6cff62f68fec6b496a6ef7b6fe6b180270b0a4ab
Josue <Josue_0096@live.it>
2022-10-31 19:24:07 +0100

    implementazione delle carte-cocktails con immagine e titolo dinamiche

--- html_css/the_wiki_cocktail/script.js
+++ html_css/the_wiki_cocktail/script.js
@@ -1,9 +1,72 @@
-fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
-
+let button = document.getElementById("button");
+let bottone = null;
+let url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
+fetch(url)
     .then(response => response.json())
     .then(json => {
-        const result = json;
-        console.log(result);
+        console.log(json);
+        
+        for(let i = 0; i < json.drinks.length; i++) {
+
+            const product = document.createElement("div");
+            product.className = "product";
+            product.style.backgroundImage = "url(" + json.drinks[i].strDrinkThumb + ")";
+
+            const productInfo = document.createElement("div");
+            productInfo.className = "productInfo";
+
+            const productName = document.createElement("p");
+            productName.className = "productName";
+            productName.id = "Cocktail";
+            productName.appendChild(document.createTextNode(json.drinks[i].strDrink));
+            productInfo.appendChild(productName);
+
+            const link = document.createElement("a");
+            link.href = "#";
+            const infoButton = document.createElement("div");
+            infoButton.className = "infoButton";
+            infoButton.appendChild(document.createTextNode("MAGGIORI INFO"));
+            link.appendChild(infoButton);
+            productInfo.appendChild(link);
+
+            product.appendChild(productInfo);
+            document.querySelector(".products").appendChild(product);
+        }
+    })
+
+
+/*function searchApi (button){
+    let url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?"
+    fetch(`${url}s=${button}`)
+    .then(response => response.json())
+    .then(json => {
+        let toReturn = {}
+        let array = json.drinks;
+       toReturn = array.shift();
+       console.log(toReturn);
+        let toappend = document.getElementById("infoButton");
+        toappend.addEventListener('onclick', function(){
+            let cocktailName = document.getElementById("textId");
+            let ALMENODAMMIUNDEFINED = toReturn.strDrink;
+            console.log(ALMENODAMMIUNDEFINED)
+        })
+
+
+        
+        //result.forEach((Element)=>{
+            //let modal = document.getElementById()
+        //})
 
         
     })
+}
+searchApi(button);
+
+
+
+button.addEventListener('click', function(){
+    bottone = document.getElementById("input").value;
+    let nameToSosbuite = document.getElementById("Cocktail")
+    nameToSosbuite.innerHTML = bottone
+    searchApi(bottone);
+})*/
